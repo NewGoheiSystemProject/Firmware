4c2ca47e106dc5d7330e96c9e3dff6cfa72c3c44
ShujiMikami <carbonized.pan@gmail.com>
2018-01-18 02:44:38 +0900

    testフォルダ作成

---
+++ NewGoheiSystemFW/Test/inc/peripheralManualControlTest.h
@@ -0,0 +1,13 @@
+/*
+ * peripheralManualControlTest.h
+ *
+ *  Created on: 2018/01/18
+ *      Author: Shuji
+ */
+
+#ifndef _PERIPHERALMANUALCONTROLTEST_H_
+#define _PERIPHERALMANUALCONTROLTEST_H_
+
+void GPIO_relay_anode_control_test();
+
+#endif /* _PERIPHERALMANUALCONTROLTEST_H_ */
---
+++ NewGoheiSystemFW/Test/src/peripheralManualControlTest.c
@@ -0,0 +1,13 @@
+/*
+ * peripheralManualControlTest.c
+ *
+ *  Created on: 2018/01/18
+ *      Author: Shuji
+ */
+
+#include "peripheralManualControlTest.h"
+
+void GPIO_relay_anode_control_test()
+{
+
+}
