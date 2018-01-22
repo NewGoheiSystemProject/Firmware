e11118c35b15b3f6ca49051502fbb0006c055a20
ShujiMikami <carbonized.pan@gmail.com>
2018-01-23 00:05:28 +0900

    BoardControlerモジュール追加

---
+++ NewGoheiSystemFW/UserModules/BoardControler/inc/BoardControler.h
@@ -0,0 +1,13 @@
+/*
+ * BoardControler.h
+ *
+ *  Created on: 2018/01/22
+ *      Author: Shuji
+ */
+
+#ifndef BOARDCONTROLER_INC_BOARDCONTROLER_H_
+#define BOARDCONTROLER_INC_BOARDCONTROLER_H_
+
+
+
+#endif /* BOARDCONTROLER_INC_BOARDCONTROLER_H_ */
---
+++ NewGoheiSystemFW/UserModules/BoardControler/src/BoardControler.c
@@ -0,0 +1,8 @@
+/*
+ * BoardControler.c
+ *
+ *  Created on: 2018/01/22
+ *      Author: Shuji
+ */
+
+
