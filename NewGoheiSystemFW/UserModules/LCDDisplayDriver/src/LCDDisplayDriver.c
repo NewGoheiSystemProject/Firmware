e5f1e2a31a23310dc080c990158852e228a64e75
ShujiMikami <carbonized.pan@gmail.com>
2018-02-10 01:14:58 +0900

    LCD初期化実装

--- NewGoheiSystemFW/UserModules/LCDDisplayDriver/src/LCDDisplayDriver.c
+++ NewGoheiSystemFW/UserModules/LCDDisplayDriver/src/LCDDisplayDriver.c
@@ -7,9 +7,46 @@
 #include "LCDDisplayDriver.h"
 #include "i2c.h"
 
+#define DEVICE_ADDRESS 0x7C
+#define COMMAND_ADDRESS 0x00
+#define TIME_OUT 100
+
+
 void initialize_LCDDisplayDriver()
 {
+	HAL_StatusTypeDef writeResult = I2C1_Master_MEM_WriteByte(DEVICE_ADDRESS, COMMAND_ADDRESS, 0x38, TIME_OUT);
 
+	if(writeResult == HAL_OK){
+		writeResult = I2C1_Master_MEM_WriteByte(DEVICE_ADDRESS, COMMAND_ADDRESS, 0x39, TIME_OUT);
+	}
+
+	if(writeResult == HAL_OK){
+		writeResult = I2C1_Master_MEM_WriteByte(DEVICE_ADDRESS, COMMAND_ADDRESS, 0x14, TIME_OUT);
+	}
+
+	if(writeResult == HAL_OK){
+		writeResult = I2C1_Master_MEM_WriteByte(DEVICE_ADDRESS, COMMAND_ADDRESS, 0x73, TIME_OUT);
+	}
+
+	if(writeResult == HAL_OK){
+		writeResult = I2C1_Master_MEM_WriteByte(DEVICE_ADDRESS, COMMAND_ADDRESS, 0x56, TIME_OUT);
+	}
+
+	if(writeResult == HAL_OK){
+		writeResult = I2C1_Master_MEM_WriteByte(DEVICE_ADDRESS, COMMAND_ADDRESS, 0x6C, TIME_OUT);
+	}
+
+	if(writeResult == HAL_OK){
+		writeResult = I2C1_Master_MEM_WriteByte(DEVICE_ADDRESS, COMMAND_ADDRESS, 0x38, TIME_OUT);
+	}
+
+	if(writeResult == HAL_OK){
+		writeResult = I2C1_Master_MEM_WriteByte(DEVICE_ADDRESS, COMMAND_ADDRESS, 0x01, TIME_OUT);
+	}
+
+	if(writeResult == HAL_OK){
+		writeResult = I2C1_Master_MEM_WriteByte(DEVICE_ADDRESS, COMMAND_ADDRESS, 0x0C, TIME_OUT);
+	}
 }
 void setChar_LCDDisplayDriver(char* str, uint8_t line)
 {
