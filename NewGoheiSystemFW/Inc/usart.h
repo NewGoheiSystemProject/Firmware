b137cf5de3ad0d87a69c9193787cffea3076acb2
ShujiMikami <carbonized.pan@gmail.com>
2018-10-01 04:02:59 +0900

    UARTで受信しまくる部分を作成

--- NewGoheiSystemFW/Inc/usart.h
+++ NewGoheiSystemFW/Inc/usart.h
@@ -64,7 +64,7 @@
 extern UART_HandleTypeDef huart3;
 
 /* USER CODE BEGIN Private defines */
-
+extern DMA_HandleTypeDef hdma_usart3_rx;
 /* USER CODE END Private defines */
 
 extern void _Error_Handler(char *, int);
--- NewGoheiSystemFW/Src/main.c
+++ NewGoheiSystemFW/Src/main.c
@@ -68,7 +68,6 @@
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
-
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -118,15 +117,39 @@ int main(void)
   MX_USART3_UART_Init();
 
   /* USER CODE BEGIN 2 */
-  uint8_t uartRxBuf[1000] = {0};
-  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_15, GPIO_PIN_RESET);
-  HAL_UART_Receive_DMA(&huart3, uartRxBuf, 1000);
+  volatile uint8_t uartRxBuf[1024] = {0};
+  volatile uint16_t readPos = 0;
+  volatile uint16_t writePos = 0;
 
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_15, GPIO_PIN_RESET);
+  HAL_UART_Receive_DMA(&huart3, uartRxBuf, 1024);
   HAL_Delay(500);
   HAL_GPIO_WritePin(GPIOE, GPIO_PIN_15, GPIO_PIN_SET);
 
+  volatile uint8_t receivedStrings[10][1000] = {0};
+  volatile uint16_t stringLength[10] = {0};
+  volatile uint8_t stringReadPos = 0;
+  volatile uint8_t stringWritePos = 0;
   while(1){
-	  ;
+	  if(__HAL_DMA_GET_COUNTER(&hdma_usart3_rx) < 1024){
+		  writePos = 1023 - __HAL_DMA_GET_COUNTER(&hdma_usart3_rx);
+		  int cnt = 0;
+		  while(readPos < writePos){
+			  if(uartRxBuf[readPos] != '\r' && uartRxBuf[readPos] != '\n'){
+				  receivedStrings[stringWritePos][cnt] = uartRxBuf[readPos];
+				  cnt++;
+				  stringLength[stringWritePos]++;
+			  }
+			  else if(uartRxBuf[readPos] == '\r' && uartRxBuf[readPos + 1] == '\n'){
+				  stringWritePos++;
+				  cnt = 0;
+			  }
+			  else{
+				  cnt = 0;
+			  }
+			  readPos++;
+		  }
+	  }
   }
 
   //LCDDriverTest();
