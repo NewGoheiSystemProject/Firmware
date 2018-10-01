a6dbe29b6e44e34bab269ffcbf57323a2d72b0a2
ShujiMikami <carbonized.pan@gmail.com>
2018-10-02 03:58:18 +0900

    Sourceにメンバ追加

--- NewGoheiSystemFW/UserModules/RingBuffer/Src/RingBuffer.c
+++ NewGoheiSystemFW/UserModules/RingBuffer/Src/RingBuffer.c
@@ -14,3 +14,28 @@ typedef struct{
 	uint32_t Count;
 	uint32_t* buffer;
 }RingBuffer_t;
+
+RingBufferHandle_t CreateRingBuffer(uint32_t size)
+{
+
+}
+void DisposeRingBuffer(RingBufferHandle_t handle)
+{
+
+}
+RING_BUFFER_STATE_t GetData(RingBufferHandle_t handle, uint32_t* buffer)
+{
+
+}
+RING_BUFFER_STATE_t AddData(RingBufferHandle_t handle, uint32_t data)
+{
+
+}
+uint32_t GetCount(RingBufferHandle_t handle)
+{
+
+}
+void ClearBuffer(RingBufferHandle_t handle)
+{
+
+}
